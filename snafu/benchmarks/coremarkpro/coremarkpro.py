c35eec1eea58166b867e6b3b25952e1557147875
Edwin Chong <5728235+ewchong@users.noreply.github.com>
2021-10-08 17:04:39 -0400

    fix precommit

--- snafu/benchmarks/coremarkpro/coremarkpro.py
+++ snafu/benchmarks/coremarkpro/coremarkpro.py
@@ -1,89 +1,108 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Runs CoreMark Pro."""
-from datetime import datetime
-import pprint
-import shlex
-import uuid
-from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
-from snafu.config import ConfigArgument
-from snafu.benchmarks import Benchmark, BenchmarkResult
-from snafu.process import sample_process
-import subprocess
 import os
 import re
+import shlex
+import uuid
+from datetime import datetime
+from typing import Dict, Iterable
+
+from dateutil import tz
+
+from snafu.benchmarks import Benchmark, BenchmarkResult
+from snafu.config import ConfigArgument
+from snafu.process import sample_process
 
 
 class Coremarkpro(Benchmark):
-    """ Wrapper for CoreMark Pro"""
+    """Wrapper for CoreMark Pro"""
 
-    """ Set for Benchmark Class """
+    # Set for Benchmark Class
     tool_name = "coremark-pro"
     args = (
-            ConfigArgument(
-                "-p",
-                "--path",
-                dest="path",
-                type=str,
-                help="Path to the coremark-pro's directory",
-                required=True,
-                ),
-            ConfigArgument(
-                "-c",
-                "--context",
-                dest="context",
-                type=int,
-                help="CoreMark Pro's context",
-                default=0,
-                required=False,
-                ),
-            ConfigArgument(
-                "-w",
-                "--workloads",
-                help="CoreMark Pro's workloads",
-                dest="workload",
-                type=int,
-                default=0,
-                required=False,
-                ),
-            ConfigArgument(
-                "-s",
-                "--sample",
-                dest="sample",
-                env_var="SAMPLE",
-                default=1,
-                type=int,
-                help="Number of times to run the benchmark",
-                required=False,),
-            ConfigArgument(
-                "-u",
-                "--upload",
-                dest="upload",
-                default=False,
-                type=bool,
-                help="Parse results from previous CoreMark Run",
-                required=False,
-                ),
-            )
+        ConfigArgument(
+            "-p",
+            "--path",
+            dest="path",
+            type=str,
+            help="Path to the coremark-pro's directory",
+            required=True,
+        ),
+        ConfigArgument(
+            "-c",
+            "--context",
+            dest="context",
+            type=int,
+            help="CoreMark Pro's context",
+            default=1,
+            required=False,
+        ),
+        ConfigArgument(
+            "-w",
+            "--workloads",
+            help="CoreMark Pro's workloads",
+            dest="workload",
+            type=int,
+            default=0,
+            required=False,
+        ),
+        ConfigArgument(
+            "-s",
+            "--sample",
+            dest="sample",
+            env_var="SAMPLE",
+            default=1,
+            type=int,
+            help="Number of times to run the benchmark",
+            required=False,
+        ),
+        ConfigArgument(
+            "-u",
+            "--upload",
+            dest="upload",
+            default=False,
+            type=bool,
+            help="Parse results from previous CoreMark Run",
+            required=False,
+        ),
+    )
 
-    result_config = {}
-
+    result_config: Dict = {}
 
     """ Helper functions"""
 
-    def build_workload_cmd(self):
-        xcmd = ""
-        if getattr(self.config, "context"):
-            xcmd += f" -c{self.config.context}"
-        if getattr(self.config, "workload"):
-            xcmd += f" -w{self.config.workload} "
+    def build_workload_cmd(self) -> list[str]:
+        """
+        Builds the command line arguments needed to run CoreMark Pro
+        """
+
+        xcmd = f" -c{self.config.context} -w{self.config.workload}"
         return shlex.split(f"make TARGET=linux64 certify-all XCMD='{xcmd}'")
 
     def create_raw_results(self) -> Iterable[BenchmarkResult]:
-        headers = ['uid', 'suite', 'name', 'ctx', 'wrk', 'fails', 't(s)', 'iter', 'iter/s', 'codesize', 'datasize']
+        """
+        Parses the raw results logs from CoreMark Pro into a Benchmark result. Ignores any median results.
+        """
+
+        headers = [
+            "uid",
+            "suite",
+            "name",
+            "ctx",
+            "wrk",
+            "fails",
+            "t(s)",
+            "iter",
+            "iter/s",
+            "codesize",
+            "datasize",
+        ]
         types = [str, str, str, int, int, int, float, int, float, int, int]
 
-        with open(self.config.path + 'builds/linux64/gcc64/logs/linux64.gcc64.log', 'rt') as file:
+        with open(
+            self.config.path + "builds/linux64/gcc64/logs/linux64.gcc64.log", "rt", encoding="utf-8"
+        ) as file:
             results = []
             prev_run_type = ""
             run_type = ""
@@ -92,40 +111,43 @@ class Coremarkpro(Benchmark):
             for line in file:
                 # Look for the following string in the logs:
                 #    Results for `run_type` started at `timestamp`
-                result = re.search(r'^#Results for (\w+) .* (\d+:\d+:\d+:\d+) XCMD', line)
+                result = re.search(r"^#Results for (\w+) .* (\d+:\d+:\d+:\d+) XCMD", line)
                 if result:
                     prev_run_type = run_type
-                    (run_type, run_starttime) = result.group(1,2)
+                    (run_type, run_starttime) = result.group(1, 2)
                     continue
 
                 # Ignore median results since it can be derived
-                if 'median' not in line:
-                    if re.search(r'^\d+', line):
+                if "median" not in line:
+                    if re.search(r"^\d+", line):
                         if prev_run_type != run_type:
                             run_index = 0
                             prev_run_type = run_type
-                        cols = re.split(r'\s+', line.rstrip())
+                        cols = re.split(r"\s+", line.rstrip())
                         converted_cols = [func(val) for func, val in zip(types, cols)]
                         record = dict(zip(headers, converted_cols))
-                        record['type'] = run_type
-                        record['starttime'] = self.convert_coremark_timestamp(run_starttime)
-                        record['run_index'] = run_index
+                        record["type"] = run_type
+                        record["starttime"] = self.convert_coremark_timestamp(run_starttime)
+                        record["run_index"] = run_index
                         run_index += 1
                         results.append(record)
                         yield self.create_new_result(
-                                data=record,
-                                config=self.result_config,
-                                tag="raw",
-                                )
-
-
+                            data=record,
+                            config=self.result_config,
+                            tag="raw",
+                        )
 
     def create_summary_results(self) -> Iterable[BenchmarkResult]:
+        """
+        Parses the CoreMark Pro's 'mark' file which has the scores calculated
+        """
 
-        headers = ['name',  'multicore', 'singlecore', 'scaling']
+        headers = ["name", "multicore", "singlecore", "scaling"]
         types = [str, float, float, float]
 
-        with open(self.config.path + 'builds/linux64/gcc64/logs/linux64.gcc64.mark', 'rt') as file:
+        with open(
+            self.config.path + "builds/linux64/gcc64/logs/linux64.gcc64.mark", "rt", encoding="utf-8"
+        ) as file:
             table_name = ""
             for line in file:
                 line = line.rstrip()
@@ -134,35 +156,36 @@ class Coremarkpro(Benchmark):
 
                 # Find where the table starts and skips the fluff
                 if "RESULTS TABLE" in line:
-                    table_name = line.split(' ')[0].lower()
+                    table_name = line.split(" ")[0].lower()
                     while True:
                         # Exit out of loop once it finds the table delimiter
-                        if('---' in next(file)):
+                        # Disable pylint false positive, doesn't impact the generator
+                        # pylint: disable=stop-iteration-return
+                        if "---" in next(file):
                             break
                     continue
 
-                cols = re.split(r'\s+', line.rstrip())
+                cols = re.split(r"\s+", line.rstrip())
                 converted_cols = [func(val) for func, val in zip(types, cols)]
                 record = dict(zip(headers, converted_cols))
                 record["type"] = table_name
 
                 yield self.create_new_result(
-                        data=record,
-                        config=self.result_config,
-                        tag="summary",
-                        )
+                    data=record,
+                    config=self.result_config,
+                    tag="summary",
+                )
 
+    @staticmethod
+    def convert_coremark_timestamp(timestamp) -> str:
+        """
+        Converts CoreMark Pro's timestamp in the raw logs into a ES friendly date format
+        """
 
+        time_obj = datetime.strptime(timestamp, "%y%j:%H:%M:%S")
+        utc_tz = tz.gettz("UTC")
 
-    def convert_coremark_timestamp(self, timestamp):
-        from dateutil import tz
-        time_obj = datetime.strptime(timestamp, '%y%j:%H:%M:%S')
-        utc_tz = tz.gettz('UTC')
-
-        return (time_obj.astimezone(utc_tz)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")   
-
-
-    """ Override member functions from Benchmark class"""
+        return (time_obj.astimezone(utc_tz)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
     def setup(self) -> bool:
 
@@ -177,10 +200,10 @@ class Coremarkpro(Benchmark):
         if "clustername" not in os.environ:
             self.config.cluster_name = "mycluster"
 
-        self.result_config['test_config'] = {
-                "workload": self.config.workload,
-                "context": self.config.context,
-                }
+        self.result_config["test_config"] = {
+            "workload": self.config.workload,
+            "context": self.config.context,
+        }
 
         return True
 
@@ -189,29 +212,23 @@ class Coremarkpro(Benchmark):
         cmd = self.build_workload_cmd()
 
         if not self.config.upload:
-            samples = sample_process(
-                    cmd,
-                    self.logger,
-                    retries=2,
-                    expected_rc=0,
-                    cwd=self.config.path
-                    )
+            samples = sample_process(cmd, self.logger, retries=2, expected_rc=0, cwd=self.config.path)
 
             for sample_num, sample in enumerate(samples):
+                self.logger.info(f"Starting coremark-pro sample number {sample_num}")
 
+                self.result_config["sample_starttime"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+                self.result_config["sample"] = self.config.sample
                 if not sample.success:
                     self.logger.critical(f"Failed to run! Got results: {sample}")
-                    return
                 else:
                     yield from self.create_raw_results()
                     yield from self.create_summary_results()
         else:
-            sample_starttime = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-            self.result_config['sample_starttime'] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-            self.result_config['sample'] = self.config.sample
+            self.result_config["sample_starttime"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            self.result_config["sample"] = self.config.sample
             yield from self.create_raw_results()
             yield from self.create_summary_results()
-
 
     def cleanup(self):
         return True
